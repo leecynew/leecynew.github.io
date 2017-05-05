8c61c97f125a174dd460f18d35cfcd72157800f1
leecynew <leecynew@163.com>
2017-05-05 16:08:04 +0800

    upload

---
+++ 10X/js/index.js
@@ -0,0 +1,14 @@
+//获取窗口宽度
+documentWidth = window.screen.availWidth;
+
+$(document).ready(function(){
+    if(documentWidth > 500){
+        documentWidth = 500; 
+    }
+    $('#container').css('width',0.95*documentWidth);
+});
+
+//游戏规则
+function rule(){
+    $('#info').toggle();
+}
